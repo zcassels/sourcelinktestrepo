ecf046a7cd71457f9a8f642aaf359e5de4508370
zcassels <zcassels@gmail.com>
2017-09-27 15:38:58 -0400

    initial commit

---
+++ Deleteme/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestCore;
+using TestCore.ResultReporting;
+
+namespace Deleteme
+{
+    [FeatureId("Feature A")]
+    [ProductId("Product")]
+    [Category("some category")]
+    public class Class1 : BaseTest
+    {
+        [TestId("Some test")]
+        public void test()
+        {
+            
+        }
+
+        protected override string Release()
+        {
+            return "foobar";
+        }
+    }
+}
